901c743994cef7ed13adeec7da07aee726b2f0a2
5adiyah <sadiali@pdx.edu>
2016-03-02 20:49:51 -0800

    Add functionality

--- scripts.js
+++ scripts.js
@@ -1,14 +1,23 @@
 var isItPrime = function(number) {
-  if((number === 1) || (number % 2 === 0) || (number % 3 === 0) || (number % 3 === 0) || (number % 5 === 0) || (number % 7 === 0)){
+  if((number === 1) || ((number % 2 === 0) && (number != 2)) || ((number % 3 === 0) && (number != 3)) || ((number % 4 === 0) && (number != 4)) || ((number % 5 === 0) && (number != 5)) || ((number % 7 === 0) && (number != 7))){
     return false;
-  } else if (number === 2){
-    return true;
   } else {
     return true;
   }
 };
 
 $(document).ready(function() {
+    $("button#check").click(function(){
+      $("#primetester").hide();
+      $("#clear").show();
+    });
+
+    $("button#clear").click(function(){
+      $("#primetester").show();
+      $("#clear").hide();
+      $("#result").hide();
+    });
+
   $("#primetester").submit(function(event) {
     var number = parseInt($("input#number").val());
     var result = isItPrime(number);
@@ -17,10 +26,11 @@ $(document).ready(function() {
       $(".sentence").text("NOT PRIME");
     } else {
       $(".not").text("PRIME!");
-      alert("prime");
+      $(".sentence").text("PRIME");
     }
 
     $("#result").show();
     event.preventDefault();
   });
+
 });
